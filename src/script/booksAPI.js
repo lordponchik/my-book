001addbb7392040c0705fc3f27b239134fdb896e
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-11-16 13:50:28 +0100

    add fn fetchGenre

--- src/script/booksAPI.js
+++ src/script/booksAPI.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://www.googleapis.com/books/v1/';
 
 export async function fetchBooksCategory(name) {
-  const params = `volumes?q=intitle:${name}&printType=books&startIndex=${getRenderStartIndex()}&maxResults=6`;
+  const params = `volumes?q=intitle:${name}&printType=books&startIndex=${getRenderStartIndex(
+    11
+  )}&maxResults=6`;
   const response = await axios.get(params);
   const data = await response.data;
   const items = await data.items;
@@ -17,7 +19,17 @@ export async function fetchBook(id) {
 
   return data;
 }
+export async function fetchGenre(subject) {
+  const params = `volumes?q=${subject}&printType=books&startIndex=${getRenderStartIndex(
+    500
+  )}&maxResults=10`;
+  const response = await axios.get(params);
+  const data = await response.data;
+  const items = await data.items;
 
-function getRenderStartIndex() {
-  return Math.floor(Math.random() * 11);
+  return items;
+}
+
+function getRenderStartIndex(max) {
+  return Math.floor(Math.random() * max);
 }
