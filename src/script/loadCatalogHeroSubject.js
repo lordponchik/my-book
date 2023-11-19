638fa3f14b56eef29b09e6e4e7a2917739473931
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-11-19 16:14:53 +0100

    add loadCatalogHeroSubject

---
+++ src/script/loadCatalogHeroSubject.js
@@ -0,0 +1,26 @@
+import { fetchGenre } from './booksAPI';
+import { renderBooks } from './renderBooks';
+
+const genreEl = document.querySelector('.hero__wrapper--genre');
+const genreBtnEl = document.querySelector('.category');
+let activeСurrentBtnEl = genreBtnEl.querySelector('.category__btn.active');
+
+fetchGenre('love').then(data => {
+  genreEl.innerHTML = renderBooks(data, 'genre', true);
+});
+
+genreBtnEl.addEventListener('click', renderCategory);
+
+function renderCategory(e) {
+  if (e.target.nodeName !== 'BUTTON') return;
+
+  activeСurrentBtnEl.classList.remove('active');
+  const activeNewBtnEl = e.target;
+  activeNewBtnEl.classList.add('active');
+  activeСurrentBtnEl = activeNewBtnEl;
+  genreEl.innerHTML = '';
+
+  fetchGenre(e.target.getAttribute('data-name')).then(data => {
+    genreEl.innerHTML = renderBooks(data, 'genre', true);
+  });
+}
