1ee1a9d2077d2c6f3c70d27726a82ac0a2871e51
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-01-31 19:19:49 +0100

    fix render book after clicking on randomBtn loadLibraryHero.js

--- src/script/loadLibraryHero.js
+++ src/script/loadLibraryHero.js
@@ -21,10 +21,11 @@ randomBtnEl.addEventListener('click', e => {
   heroEl.innerHTML = '';
 
   fetchRandomBook(randomName()).then(item => {
-    const obj_book = new objBook(item[0]);
+    const obj_book = [];
+    obj_book.push(new objBook(item[0]));
 
     heroEl.innerHTML = renderBooks(item[0], 'random-book', true, true, obj_book);
-    obj_book.addEvent();
+    obj_book[0].addEvent();
   });
 });
 
