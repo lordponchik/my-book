55c049cd700351968c929ca1f4ba874cab22abb5
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-02-09 13:01:53 +0100

    add handling an error from a request from the server for catalogHero and libraryHero

--- src/script/loadCatalogHeroSubject.js
+++ src/script/loadCatalogHeroSubject.js
@@ -6,14 +6,20 @@ const genreEl = document.querySelector('.hero__wrapper--genre');
 const genreBtnEl = document.querySelector('.category');
 let activeСurrentBtnEl = genreBtnEl.querySelector('.category__btn.active');
 
-fetchGenre('liebe').then(data => {
-  const obj_book = [];
-  data.map(el => obj_book.push(new objBook(el)));
+fetchGenre('liebe')
+  .then(data => {
+    const obj_book = [];
+    data.map(el => obj_book.push(new objBook(el)));
 
-  genreEl.innerHTML = renderBooks(data, 'genre', true, false, obj_book);
+    genreEl.innerHTML = renderBooks(data, 'genre', true, false, obj_book);
 
-  obj_book.forEach(element => element.addEvent());
-});
+    obj_book.forEach(element => element.addEvent());
+  })
+  .catch(error => {
+    genreEl.innerHTML = `<p style="align-self:center;margin:0 auto;text-align:center;color:var(--secondary-text-color);">OOPS... <br />
+We are very sorry!<br />
+We have lost the results from the server, please reload the page</p>`;
+  });
 
 genreBtnEl.addEventListener('click', renderCategory);
 
@@ -26,7 +32,13 @@ function renderCategory(e) {
   activeСurrentBtnEl = activeNewBtnEl;
   genreEl.innerHTML = '';
 
-  fetchGenre(e.target.getAttribute('data-name')).then(data => {
-    genreEl.innerHTML = renderBooks(data, 'genre', true);
-  });
+  fetchGenre(e.target.getAttribute('data-name'))
+    .then(data => {
+      genreEl.innerHTML = renderBooks(data, 'genre', true);
+    })
+    .catch(error => {
+      genreEl.innerHTML = `<p style="align-self:center;margin:0 auto;text-align:center;color:var(--secondary-text-color);">OOPS... <br />
+We are very sorry!<br />
+We have lost the results from the server, please reload the page</p>`;
+    });
 }
--- src/script/loadLibraryHero.js
+++ src/script/loadLibraryHero.js
@@ -6,28 +6,40 @@ import { objBook } from './localList';
 const heroEl = document.querySelector('.rndbook');
 const randomBtnEl = document.querySelector('.btn-random');
 
-fetchRandomBook(randomName()).then(item => {
-  const obj_book = [];
-  obj_book.push(new objBook(item[0]));
-
-  heroEl.innerHTML = renderBooks(item[0], 'random-book', true, true, obj_book);
-
-  obj_book[0].addEvent();
-});
-
-randomBtnEl.addEventListener('click', e => {
-  e.preventDefault();
-
-  heroEl.innerHTML = '';
-
-  fetchRandomBook(randomName()).then(item => {
+fetchRandomBook(randomName())
+  .then(item => {
     const obj_book = [];
     obj_book.push(new objBook(item[0]));
 
     heroEl.innerHTML = renderBooks(item[0], 'random-book', true, true, obj_book);
+
     obj_book[0].addEvent();
+  })
+  .catch(error => {
+    genreEl.innerHTML = `<p style="align-self:center;margin:0 auto;text-align:center;color:var(--secondary-text-color);">OOPS... <br />
+We are very sorry!<br />
+We have lost the results from the server, please reload the page</p>`;
   });
-});
+
+randomBtnEl
+  .addEventListener('click', e => {
+    e.preventDefault();
+
+    heroEl.innerHTML = '';
+
+    fetchRandomBook(randomName()).then(item => {
+      const obj_book = [];
+      obj_book.push(new objBook(item[0]));
+
+      heroEl.innerHTML = renderBooks(item[0], 'random-book', true, true, obj_book);
+      obj_book[0].addEvent();
+    });
+  })
+  .catch(error => {
+    genreEl.innerHTML = `<p style="align-self:center;margin:0 auto;text-align:center;color:var(--secondary-text-color);">OOPS... <br />
+We are very sorry!<br />
+We have lost the results from the server, please reload the page</p>`;
+  });
 
 function randomName() {
   return dictionary[Math.floor(Math.random() * dictionary.length)];
