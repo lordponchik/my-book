5d5ff8b385348149aba1463b98a4bb7a7bb234a1
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-02-09 12:28:19 +0100

    add handling an error from a request from the server for loadHomeSeries.js

--- src/script/loadHomeSeries.js
+++ src/script/loadHomeSeries.js
@@ -9,23 +9,47 @@ const icefireEl = document.querySelector('.icefire');
 const witcherEl = document.querySelector('.witcher');
 const darkTowerEl = document.querySelector('.dark-tower');
 
-fetchBooksCategory('harry+potter').then(items => {
-  potterEl.innerHTML = renderBooks(items, 'series');
-  addSlider('potter', 'series');
-});
+fetchBooksCategory('harry+potter')
+  .then(items => {
+    potterEl.innerHTML = renderBooks(items, 'series');
+    addSlider('potter', 'series');
+  })
+  .catch(error => {
+    potterEl.innerHTML = `<p style="align-self:center;margin:0 auto;text-align:center;color:var(--secondary-text-color);">OOPS... <br />
+We are very sorry!<br />
+We have lost the results from the server, please reload the page</p>`;
+  });
 
-fetchBooksCategory('a+song+of+ice+and+fire').then(items => {
-  icefireEl.innerHTML = renderBooks(items, 'series');
-  addSlider('icefire', 'series');
-});
-fetchBooksCategory('the+witcher').then(items => {
-  witcherEl.innerHTML = renderBooks(items, 'series');
-  addSlider('witcher', 'series');
-});
-fetchBooksCategory('the+dark+tower').then(items => {
-  darkTowerEl.innerHTML = renderBooks(items, 'series');
-  addSlider('dark-tower', 'series');
-});
+fetchBooksCategory('a+song+of+ice+and+fire')
+  .then(items => {
+    icefireEl.innerHTML = renderBooks(items, 'series');
+    addSlider('icefire', 'series');
+  })
+  .catch(error => {
+    potterEl.innerHTML = `<p style="align-self:center;margin:0 auto;text-align:center;color:var(--secondary-text-color);">OOPS... <br />
+We are very sorry!<br />
+We have lost the results from the server, please reload the page</p>`;
+  });
+fetchBooksCategory('the+witcher')
+  .then(items => {
+    witcherEl.innerHTML = renderBooks(items, 'series');
+    addSlider('witcher', 'series');
+  })
+  .catch(error => {
+    potterEl.innerHTML = `<p style="align-self:center;margin:0 auto;text-align:center;color:var(--secondary-text-color);">OOPS... <br />
+We are very sorry!<br />
+We have lost the results from the server, please reload the page</p>`;
+  });
+fetchBooksCategory('the+dark+tower')
+  .then(items => {
+    darkTowerEl.innerHTML = renderBooks(items, 'series');
+    addSlider('dark-tower', 'series');
+  })
+  .catch(error => {
+    potterEl.innerHTML = `<p style="align-self:center;margin:0 auto;text-align:center;color:var(--secondary-text-color);">OOPS... <br />
+We are very sorry!<br />
+We have lost the results from the server, please reload the page</p>`;
+  });
 
 seriesEl.forEach(el => {
   el.addEventListener('click', modalShow, true);
